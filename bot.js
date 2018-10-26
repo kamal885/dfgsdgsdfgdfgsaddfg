eb6f1af1e3885d3ef5be10b9398597bdd3c51b73
kamal885 <43897372+kamal885@users.noreply.github.com>
2018-10-26 23:43:29 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,23 +1,32 @@
-const Discord = require("discord.js")
-const client = new Discord.Client();
-const YTDL = require('ytdl-core');
-const nodeopus = require('node-opus');
-const ffmpeg = require('ffmpeg');
-var servers = {};
-function play(connection, message, args) {
-  var server = servers[message.guild.id];
-  server.dispatcher = connection.playStream(YTDL(args[0]), {filter: "audioonly"});
-  server.queue.shift();
-  server.dispatcher.on("end", function() {
-    if (server.queue[0]) play(connection, message);
-    else connection.disconnect();
-  });
-}
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`PANDA BOT STARTED`);
+        console.log(`---------------`);
+      console.log(`ON ${client.guilds.size} Servers `);
+    console.log(`---------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setGame(`33.`,"http://twitch.tv/y04zgamer")
+   client.user.setStatus("dnd")
+});
 
 
-client.on('message', message =>{
-  if(message.content.startsWith('join')){
-    const voiceChannel = message.member.voiceChannel
-    voiceChannel.join();
-    message.channel.send("تم الأتصال بالروم الصوتي")
-}})
+
+
+
+
+
+
+
+	
+	
+	
+	
+	
+	
+
+	
+	
+	
+
+client.login(process.env.BOT_TOKEN);
